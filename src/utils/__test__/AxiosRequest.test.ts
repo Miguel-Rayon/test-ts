b7b2ef31662c5cbd6928fbe93b8521dd8deaec73
Jose Miguel <miguel.hdz.rayon@gmail.com>
2023-04-12 18:28:58 -0600

    cambios en axios

--- src/utils/__test__/AxiosRequest.test.ts
+++ src/utils/__test__/AxiosRequest.test.ts
@@ -2,11 +2,32 @@ import axios from "axios";
 import moxios from "moxios";
 import { fecthAlbum } from "../axiosRequests";
 
-const config = { headers: { "Accept-Encoding": "gzip,deflate,compress" } };
-
 jest.useRealTimers();
 
 let axiosInstance: any;
+
+const album = [
+  {
+    userId: 1,
+    id: 1,
+    title: "quidem molestiae enim",
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "sunt qui excepturi placeat culpa",
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "omnis laborum odio",
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: "non esse culpa molestiae omnis sed optio",
+  },
+];
 
 describe("fecthAlbum", () => {
   describe("when API call is successful", () => {
@@ -14,40 +35,17 @@ describe("fecthAlbum", () => {
       moxios.install();
       axiosInstance = axios.create();
       moxios.install(axiosInstance);
-    });
-
-    afterEach(function () {
-      moxios.uninstall();
-    });
-    it("should return album list", async () => {
-      const album = [
-        {
-          userId: 1,
-          id: 1,
-          title: "quidem molestiae enim",
-        },
-        {
-          userId: 1,
-          id: 2,
-          title: "sunt qui excepturi placeat culpa",
-        },
-        {
-          userId: 1,
-          id: 3,
-          title: "omnis laborum odio",
-        },
-        {
-          userId: 1,
-          id: 4,
-          title: "non esse culpa molestiae omnis sed optio",
-        },
-      ];
 
       moxios.stubRequest("https://jsonplaceholder.typicode.com/albums", {
         status: 200,
-        data: album,
+        response: album,
       });
+    });
 
+    afterEach(function () {
+      moxios.uninstall(axiosInstance);
+    });
+    it("should return album list", async () => {
       const responseAlbum = await axiosInstance.get(
         "https://jsonplaceholder.typicode.com/albums"
       );
